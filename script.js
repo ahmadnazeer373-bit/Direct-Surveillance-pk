f38e3b687721f100dcf063ec852c4c1e12c5c334
ahmadnazeer373-bit <ahmadnazeer373@gmail.com>
2026-07-25 00:34:57 +0500

    Update script.js

--- script.js
+++ script.js
@@ -73,3 +73,34 @@ document.querySelectorAll(".nav-menu a").forEach(link => {
     });
 
 });
+/*======================================
+        BACK TO TOP
+======================================*/
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 400) {
+
+        backToTop.classList.add("show");
+
+    } else {
+
+        backToTop.classList.remove("show");
+
+    }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
