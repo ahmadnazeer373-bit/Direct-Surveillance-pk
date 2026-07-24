76106b0469a86f9ad76bcf44c9f25ca5d9858c17
ahmadnazeer373-bit <ahmadnazeer373@gmail.com>
2026-07-25 02:28:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -104,3 +104,37 @@ backToTop.addEventListener("click", () => {
     });
 
 });
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+    counter.innerText = "0";
+
+    const updateCounter = () => {
+
+        const target = +counter.getAttribute("data-target");
+
+        const current = +counter.innerText;
+
+        const increment = target / 100;
+
+
+        if(current < target){
+
+            counter.innerText = Math.ceil(current + increment);
+
+            setTimeout(updateCounter, 20);
+
+        } 
+        else {
+
+            counter.innerText = target;
+
+        }
+
+    };
+
+
+    updateCounter();
+
+});
